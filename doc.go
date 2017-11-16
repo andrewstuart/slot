f5989e502392d5156f71b37d384fddea3798a2ec
Andrew Stuart <andrew.stuart2@gmail.com>
2017-11-15 23:01:45 -0700

    Add top-level docs

---
+++ doc.go
@@ -0,0 +1,5 @@
+// package slot gives some helpful abstractions over the nlopes/slack RTM
+// integrations. Most use cases are intended to be made easier. The common bot
+// response abstraction is the Responder. Many implementations will be created
+// to assist in most of the common bot use cases.
+package slot
